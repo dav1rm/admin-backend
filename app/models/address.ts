4131d731d00fcc9af16dad3e3f7430c85bacab65
Davi Rodrigues <drmedeiros.gok@rd.com.br>
2024-03-02 20:04:00 -0300

    feat: add address model and migration

---
+++ app/models/address.ts
@@ -0,0 +1,31 @@
+import { DateTime } from 'luxon'
+import { BaseModel, column } from '@adonisjs/lucid/orm'
+
+export default class Address extends BaseModel {
+  @column({ isPrimary: true })
+  declare id: number
+
+  @column()
+  declare postcode: string
+
+  @column()
+  declare street: string
+
+  @column()
+  declare number: number
+
+  @column()
+  declare neighborhood: string
+
+  @column()
+  declare city: string
+
+  @column()
+  declare state: string
+
+  @column.dateTime({ autoCreate: true })
+  declare createdAt: DateTime
+
+  @column.dateTime({ autoCreate: true, autoUpdate: true })
+  declare updatedAt: DateTime
+}
---
+++ database/migrations/1709419473699_create_addresses_table.ts
@@ -0,0 +1,25 @@
+import { BaseSchema } from '@adonisjs/lucid/schema'
+
+export default class extends BaseSchema {
+  protected tableName = 'addresses'
+
+  async up() {
+    this.schema.createTable(this.tableName, (table) => {
+      table.increments('id')
+      table.integer('user_id').unsigned().references('users.id').onDelete('CASCADE')
+      table.string('postcode').notNullable()
+      table.string('street').notNullable()
+      table.integer('number').notNullable()
+      table.string('neighborhood').nullable()
+      table.string('city').notNullable()
+      table.string('state').notNullable()
+
+      table.timestamp('created_at')
+      table.timestamp('updated_at')
+    })
+  }
+
+  async down() {
+    this.schema.dropTable(this.tableName)
+  }
+}
